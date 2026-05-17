82f14f38d9ddd20e2725e2941e6e6029902b0e91
alinabez700-afk <alinabez700@gmail.com>
2026-05-17 10:33:30 -0400

    Complete Lab 7: Fixed camera artifacts and UI

---
+++ build/CMakeFiles/opencv_lab7.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_lab7.dir/link.d"
+  "CMakeFiles/opencv_lab7.dir/src/CameraProvider.cpp.o"
+  "CMakeFiles/opencv_lab7.dir/src/CameraProvider.cpp.o.d"
+  "CMakeFiles/opencv_lab7.dir/src/FaceDetector.cpp.o"
+  "CMakeFiles/opencv_lab7.dir/src/FaceDetector.cpp.o.d"
+  "CMakeFiles/opencv_lab7.dir/src/FrameProcessor.cpp.o"
+  "CMakeFiles/opencv_lab7.dir/src/FrameProcessor.cpp.o.d"
+  "CMakeFiles/opencv_lab7.dir/src/KeyProcessor.cpp.o"
+  "CMakeFiles/opencv_lab7.dir/src/KeyProcessor.cpp.o.d"
+  "CMakeFiles/opencv_lab7.dir/src/main.cpp.o"
+  "CMakeFiles/opencv_lab7.dir/src/main.cpp.o.d"
+  "opencv_lab7"
+  "opencv_lab7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_lab7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/opencv_lab7.dir/src/KeyProcessor.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/opencv_lab7.dir/src/KeyProcessor.cpp.o: \
+ /home/kali/dpat_lab7/src/KeyProcessor.cpp /usr/include/stdc-predef.h \
+ /home/kali/dpat_lab7/include/KeyProcessor.hpp
